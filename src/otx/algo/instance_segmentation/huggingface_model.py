1262c9d425b7160308d3d7ce52fbb215564ede6e
Eugene Liu <eugene.liu@intel.com>
2024-08-01 17:44:53 +0100

    update

--- src/otx/algo/instance_segmentation/huggingface_model.py
+++ src/otx/algo/instance_segmentation/huggingface_model.py
@@ -5,16 +5,15 @@
 
 from __future__ import annotations
 
+from types import MethodType
 from typing import TYPE_CHECKING, Any
 
 import numpy as np
 import torch
 from torch import Tensor, nn
 from torchvision import tv_tensors
+from transformers import AutoModelForUniversalSegmentation
 from transformers.models.mask2former.modeling_mask2former import (
-    Mask2FormerConfig,
-    Mask2FormerForUniversalSegmentation,
-    Mask2FormerLoss,
     dice_loss,
     sample_point,
     sigmoid_cross_entropy_loss,
@@ -36,63 +35,56 @@ if TYPE_CHECKING:
     from otx.core.metrics import MetricCallable
 
 
-class OTXMask2FormerLoss(Mask2FormerLoss):
-    def select_masks(self, tgt_idx, mask_labels):
-        gt_masks = []
-        batch_size = torch.max(tgt_idx[0]) + 1
-        for b in range(batch_size):
-            gt_masks.append(mask_labels[b][tgt_idx[1][tgt_idx[0] == b]])
-        return torch.cat(gt_masks, dim=0)
-
-    def loss_masks(
-        self,
-        masks_queries_logits: torch.Tensor,
-        mask_labels: list[torch.Tensor],
-        indices: tuple[np.array],
-        num_masks: int,
-    ) -> dict[str, torch.Tensor]:
-        src_idx = self._get_predictions_permutation_indices(indices)
-        tgt_idx = self._get_targets_permutation_indices(indices)
-        # shape (batch_size * num_queries, height, width)
-        pred_masks = masks_queries_logits[src_idx]
-        # shape (batch_size, num_queries, height, width)
-
-        # NOTE: instead of padding the masks, we select the masks without creating unnecessary masks
-        target_masks = self.select_masks(tgt_idx, mask_labels)
-
-        # No need to upsample predictions as we are using normalized coordinates
-        pred_masks = pred_masks[:, None]
-        target_masks = target_masks[:, None]
-
-        # Sample point coordinates
-        with torch.no_grad():
-            point_coordinates = self.sample_points_using_uncertainty(
-                pred_masks,
-                lambda logits: self.calculate_uncertainty(logits),
-                self.num_points,
-                self.oversample_ratio,
-                self.importance_sample_ratio,
-            )
-
-            point_labels = sample_point(target_masks.float(), point_coordinates, align_corners=False).squeeze(1)
-
-        point_logits = sample_point(pred_masks, point_coordinates, align_corners=False).squeeze(1)
-
-        losses = {
-            "loss_mask": sigmoid_cross_entropy_loss(point_logits, point_labels, num_masks),
-            "loss_dice": dice_loss(point_logits, point_labels, num_masks),
-        }
-
-        del pred_masks
-        del target_masks
-        return losses
+def select_masks(tgt_idx, mask_labels):
+    gt_masks = []
+    batch_size = torch.max(tgt_idx[0]) + 1
+    for b in range(batch_size):
+        gt_masks.append(mask_labels[b][tgt_idx[1][tgt_idx[0] == b]])
+    return torch.cat(gt_masks, dim=0)
 
 
-class OTXMask2FormerForUniversalSegmentation(Mask2FormerForUniversalSegmentation):
-    def __init__(self, config: Mask2FormerConfig):
-        # config.num_queries = 200
-        super().__init__(config)
-        self.criterion = OTXMask2FormerLoss(config=config, weight_dict=self.weight_dict)
+def loss_masks(
+    self,
+    masks_queries_logits: torch.Tensor,
+    mask_labels: list[torch.Tensor],
+    indices: tuple[np.array],
+    num_masks: int,
+) -> dict[str, torch.Tensor]:
+    src_idx = self._get_predictions_permutation_indices(indices)
+    tgt_idx = self._get_targets_permutation_indices(indices)
+    # shape (batch_size * num_queries, height, width)
+    pred_masks = masks_queries_logits[src_idx]
+    # shape (batch_size, num_queries, height, width)
+
+    # NOTE: instead of padding the masks, we select the masks without creating unnecessary masks
+    target_masks = select_masks(tgt_idx, mask_labels)
+
+    # No need to upsample predictions as we are using normalized coordinates
+    pred_masks = pred_masks[:, None]
+    target_masks = target_masks[:, None]
+
+    # Sample point coordinates
+    with torch.no_grad():
+        point_coordinates = self.sample_points_using_uncertainty(
+            pred_masks,
+            lambda logits: self.calculate_uncertainty(logits),
+            self.num_points,
+            self.oversample_ratio,
+            self.importance_sample_ratio,
+        )
+
+        point_labels = sample_point(target_masks.float(), point_coordinates, align_corners=False).squeeze(1)
+
+    point_logits = sample_point(pred_masks, point_coordinates, align_corners=False).squeeze(1)
+
+    losses = {
+        "loss_mask": sigmoid_cross_entropy_loss(point_logits, point_labels, num_masks),
+        "loss_dice": dice_loss(point_logits, point_labels, num_masks),
+    }
+
+    del pred_masks
+    del target_masks
+    return losses
 
 
 class HuggingFaceModelForInstanceSeg(ExplainableOTXInstanceSegModel):
@@ -118,12 +110,13 @@ class HuggingFaceModelForInstanceSeg(ExplainableOTXInstanceSegModel):
         )
 
     def _build_model(self, num_classes: int) -> nn.Module:
-        # TODO: change this to universal segmentation model
-        return OTXMask2FormerForUniversalSegmentation.from_pretrained(
+        model = AutoModelForUniversalSegmentation.from_pretrained(
             pretrained_model_name_or_path=self.model_name,
             num_labels=num_classes,
             ignore_mismatched_sizes=True,
         )
+        model.criterion.loss_masks = MethodType(loss_masks, model.criterion)
+        return model
 
     def _customize_inputs(self, entity: InstanceSegBatchDataEntity) -> dict[str, Any]:
         if self.training:
